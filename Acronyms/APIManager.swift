034c9310b64eb32a7a66103ff74232ee1a73e43b
Developer <developer@MacBook-Pro.attlocal.net>
2017-03-24 00:07:16 -0700

    Feat: Refactor APIManager

--- Acronyms/APIManager.swift
+++ Acronyms/APIManager.swift
@@ -6,34 +6,43 @@
 //  Copyright © 2017 Zubair. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 // MARK: Handling Search Results
 struct APIManager {
-    static var searchResults = [Meaning]()
-    // This helper method helps parse response JSON NSData into an array of Track objects.
-    static func updateSearchResultsForSearchTerm(_ data: Data?, searchTerm: String, completion: () -> Void) {
-        searchResults.removeAll()
-        do {
+    
+    private static var urlComponents = URLComponents(string: "http://www.nactem.ac.uk/software/acromine/dictionary.py")
+    private static let session = URLSession.shared
+    
+    static func requestMeanings(of abbreviation: String, completion: (([Meaning]) -> ())? = nil) -> URLSessionDataTask? {
+        let queryItem = URLQueryItem(name: "sf", value: abbreviation)
+        var components = urlComponents
+        components?.queryItems = [queryItem]
+        var meanings = [Meaning]()
+        guard let url = components?.url else {
+            completion?(meanings)
+            return nil
+        }
+        
+        let task = session.dataTask(with: url) { (data, response, error) in
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
             if let data = data, let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [[String: Any]] {
-                
                 if let jsonDict = json?.first, let longFormDicts = jsonDict["lfs"] as? [[String: Any]] {
                     for longFormDict in longFormDicts {
-                        
                         let longForm = longFormDict["lf"] as! String
-                        let meaning = Meaning(abbreviation: searchTerm, longForm: longForm)
-                        searchResults.append(meaning)
-                        completion()
-                        
+                        let meaning = Meaning(abbreviation: abbreviation, longForm: longForm)
+                        meanings.append(meaning)
                     }
                 }
             }
             
-        } catch let error as NSError {
-            print("Error parsing results: \(error.localizedDescription)")
+            completion?(meanings)
         }
-    }
-}
+        
+        task.resume()
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        return task
+    }}
 
 
 
